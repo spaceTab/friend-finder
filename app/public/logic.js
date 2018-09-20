d631e3051b61ba5d669b4d2c52ac8c8f2a23ab6f
spaceTab <93plaistj@protonmail.ch>
2018-09-19 23:27:46 -0400

    cleaning up code

--- app/public/logic.js
+++ app/public/logic.js
@@ -1,84 +1,3 @@
-// $(function () {
-//     console.log('here')
-//     $('.submit').on("click", () => {
-//         event.preventDefault();
-
-//         const VALIDATE = () => {
-//             let isValid = true;
-
-//             $(".validate").each(function () {
-//                 if (!$(this).val()) isValid = false;
-//             });
-//             $('.browser-default').each(function () {
-//                 if (!$(this).val()) isValid = false;
-//             });
-//             console.log(isValid)
-//             return isValid;
-//         }
-
-//         if (VALIDATE()) {
-
-//             let usrData = {
-//                 name: $("#name").val().trim(),
-//                 img: $("#img").val().trim()
-//             }
-
-//             let usrScores = [];
-//             console.log('here');
-//             $(".browser-default").each(function () {
-//                 usrScores.push($(this).val());
-//                 console.log($(this));
-//             }).promise().done(() => {
-//                 usrData.score = usrScores;
-
-//             })
-//             let URL = window.location.origin;
-
-//             $.post(`${URL}/api/friends`, usrData, (data) => {
-//                 console.log('data', usrData);
-//                 console.log(data);
-//                 console.log(URL);
-//                 // if (data) {
-//                 //     $('.modal-content').empty();
-//                 //     $('#matchName').val('');
-//                 //     $('#matchImg').val('');
-//                 //     $('.browser-default').each(function () {
-//                 //         $(this).val('');
-//                 //     })
-
-//                 //     data.forEach(elem => {
-//                 //         let $cheeseDiv = $('<div class="friendmatch">');
-//                 //         let $imageDiv = $('<div class="friendImg">');
-//                 //         let cheese = elem.name;
-//                 //         let url = elem.img;
-
-//                 //         let banner = $('<h2>').text(cheese);
-//                 //         let photo = $('<img>').attr('src', url)
-
-//                 //         $cheeseDiv.append(banner);
-//                 //         $imageDiv.append(photo);
-//                 //         $('.modal-content').append($cheeseDiv, $imageDiv);
-//                 //     });
-//                 // }
-//             })
-//         } else {
-//             $('.modal-content').empty();
-//             let $err = $(`<h4 class="error">You need to fill out the whole form!</h4>`);
-//             let $errDiv = $(`<div class="modal-error">`).append($err);
-//             $('.modal-content').append($errDiv);
-//         }
-
-//         // attemp to fix the 500 internal error
-
-//         // $('#btn').on("click", function () {
-//         //     location.reload();
-//         // })
-//         $('.modal').modal();
-//         $('select').formSelect();
-//     });
-// });
-
-
 $(function () {
 
     $('.submit').on('click', () => {
@@ -166,9 +85,7 @@ $(function () {
     });
 
     M.AutoInit();
-    // $('.modal').modal();
-    // $('select').formSelect();
-    // $('.parallax').parallax();
+
 });
 
 
